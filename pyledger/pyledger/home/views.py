56256337e6a1a7415c51007bf62bb14c8215f076
Mohan Chand Peddapuli <mohanroger63@gmail.com>
2018-11-30 18:48:38 +0530

    Add files via upload
    second commit

--- pyledger/pyledger/home/views.py
+++ pyledger/pyledger/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Expenses
 
 def index(request):
     user = request.user
@@ -8,16 +10,20 @@ def index(request):
 
 def addexpense(request):
     user = request.user
+    userobj = User.objects.get(username=user.username)
     if request.method == 'POST':
-        messages = []
         category = request.POST.get('category')
         spentat = request.POST.get('spentat')
         amount = request.POST.get('amount')
         modeofpayment = request.POST.get('modeofpayment')
         datespent = request.POST.get('datespent')
-        if amount < 0:
-            messages.append('Please enter an valid amount')
-            context = {'username':user.username,'messages':messages}
-            return render(request, 'home/addexpense.html', context)
+        if int(amount) < 0:
+            messages.add_message(request, messages.INFO, 'Please enter an valid amount')
+            return redirect('addexpense')
+        else:
+            trasaction = Expenses(category=category,spentat=spentat,amount=amount,modeofpayment=modeofpayment,datespent=datespent,username=userobj)
+            trasaction.save()
+            messages.add_message(request, messages.INFO, 'Your expense record has been saved')
+            return redirect('addexpense')
     context = {'username':user.username}
     return render(request, 'home/addexpense.html', context)
