8cc5663b1414a3063f855791b26446b855810e6e
unknown <mohanroger63@gmail.com>
2018-12-02 22:41:23 +0530

    Added create bill splits feature

--- pyledger/pyledger/home/urls.py
+++ pyledger/pyledger/home/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="index"),
-    path('addexpense/',views.addexpense, name="addexpense")
+    path('addexpense/',views.addexpense, name="addexpense"),
+    path('split/create/', views.createsplit, name="createsplit"),
+    path('splits/manage/<int:pk>', views.managesplits, name="managesplits"),
+    path('splits/addtransaction',views.addsplittrans, name="addsplittrans"),
 ]
--- pyledger/pyledger/home/views.py
+++ pyledger/pyledger/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .models import Expenses
+from .models import Expenses, Splitmembers, Splits, Splittransactions
 from datetime import datetime
 
 def index(request):
@@ -31,3 +31,40 @@ def addexpense(request):
             return redirect('addexpense')
     context = {'username':user.username}
     return render(request, 'home/addexpense.html', context)
+
+def createsplit(request):
+    user = request.user
+    if request.method == 'POST':
+        name = request.POST.get('groupname')
+        userobj = User.objects.get(username=user.username)
+        datecreated = datetime.now().strftime('%Y-%m-%d')
+        splitobj = Splits(username=userobj, name=name, datecreated=datecreated)
+        splitobj.save()
+        return redirect('managesplits',splitobj.id)
+    else:
+        context = {'username':user.username}
+        return render(request, 'home/createsplit.html', context=context)
+
+def managesplits(request,pk):
+    user = request.user
+    members = []
+    splitmemberobj = Splitmembers.objects.filter(splitid=pk)
+    for splitmember in splitmemberobj:
+        memberobj = User.objects.get(id=splitmember.member.id)
+        members.append(memberobj)
+    if request.method == 'POST':
+        splitobj = Splits.objects.get(id=pk)
+        member = request.POST.get('username')
+        memberobj = User.objects.get(username=member)
+        if Splitmembers.objects.filter(member=memberobj).exists():
+            messages.add_message(request, messages.INFO, "Member already exists")
+            return redirect('managesplits',pk)
+        obj = Splitmembers(splitid=splitobj, member=memberobj)
+        obj.save()
+        return redirect('managesplits',pk)
+    else:
+        context = {'username':user.username,'id':pk,'members':members}
+        return render(request, 'home/managesplits.html',context=context)
+
+def addsplittrans(request):
+    pass
