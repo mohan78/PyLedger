d15ef816707ad72e304e653e0c6bb3525d8ed3eb
unknown <mohanroger63@gmail.com>
2018-12-05 23:10:25 +0530

    db fix 5

--- pyledger/settings.py
+++ pyledger/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,9 +78,9 @@ WSGI_APPLICATION = 'pyledger.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-import dj_database_url
+
 DATABASES = { 'default': dj_database_url.config() }
-DATABASES['default'] =  dj_database_url.config()
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
