5b495a216c62a4f7fcbe1477f7ea92315f799ec9
unknown <mohanroger63@gmail.com>
2018-12-05 22:47:42 +0530

    database settings fix

--- pyledger/settings.py
+++ pyledger/settings.py
@@ -77,16 +77,6 @@ WSGI_APPLICATION = 'pyledger.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': ''
-    }
-}
 
 
 # Password validation
